7002297a4d0269ce9d5ed8c1a906a93edd99b5ac
dollae <dollaeboss@gmail.com>
2022-05-03 01:49:08 +0900

    220503

--- src/ch02/Test02.java
+++ src/ch02/Test02.java
@@ -1,6 +1,14 @@
 package ch02;
 
-public class Test02 {
+import java.util.Scanner;
 
+public class Test02 {
+    public static void main(String[] args) {
+
+        System.out.print("몇 층인지 입력하세요 >> ");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.println((a * 5) + "m 입니다.");
+    }
 
 }
