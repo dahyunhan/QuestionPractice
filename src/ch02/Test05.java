c417780463d6c7058d7a9b3799c4f0c9a4d626ff
dollae <dollaeboss@gmail.com>
2022-05-18 03:01:41 +0900

    220517

--- src/ch02/Test05.java
+++ src/ch02/Test05.java
@@ -1,1 +1,27 @@
+package ch02;
 
+import java.util.Scanner;
+
+public class Test05 {
+    public static void main(String[] srgs) {
+
+        System.out.print("논리 연산을 입력하세요 >> ");
+
+        Scanner scanner = new Scanner(System.in);
+        Boolean x = scanner.nextBoolean();
+        String b = scanner.next();
+        Boolean y = scanner.nextBoolean();
+
+        switch (b) {
+            case "AND":
+                System.out.println(x && y);
+
+                break;
+            case "OR":
+                System.out.println(x || y);
+                break;
+            default:
+                System.out.println("AND 나 OR 입력");
+            }
+        }
+    }
